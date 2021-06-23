bbfc16e4b3e1f8167dbb8b798943a0a589a7e6bc
Todor Imreorov <blurymind@gmail.com>
2021-06-23 17:40:50 +0100

    make demo a pwa

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('tilemap-editor-store').then((cache) => cache.addAll([
+        '/tilemap-editor/',
+      '/tilemap-editor/index.html',
+      '/tilemap-editor/src/tilemap-editor.js',
+      '/tilemap-editor/src/styles.css',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
