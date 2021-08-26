569084a39879b9eb4e3bf9b2eb7aa409eb3931ed
Todor Imreorov <blurymind@gmail.com>
2021-08-26 13:27:10 +0100

    add plus/minus to zoom btns

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -94,8 +94,8 @@
         <div class="tool_wrapper">
             <label id="undoBtn" title="Undo">↩️️</label>
             <label id="redoBtn" title="Redo">🔁️</label>
-            <label id="zoomIn" title="Zoom in">🔎️</label>
-            <label id="zoomOut" title="Zoom out">🔍</label>
+            <label id="zoomIn" title="Zoom in">🔎️+</label>
+            <label id="zoomOut" title="Zoom out">🔎️-</label>
             <label id="zoomLabel">️</label>
         </div>
             
