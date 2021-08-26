c3ef5b3ad006d6c06d3dcd91dcd7edbd038bc0a2
Todor Imreorov <blurymind@gmail.com>
2021-08-26 19:23:08 +0100

    make service worker use cached as a second option

--- sw.js
+++ sw.js
@@ -23,6 +23,6 @@ self.addEventListener('fetch', (e) => {
     }
     console.log(e.request.url);
     e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
+        caches.match(e.request).then((response) => fetch(e.request) || response),
     );
 });
